6bd385e2346effd073563e629e776a25e264cfec
gordonmenzies <heymish96@gmail.com>
2024-05-16 11:13:30 +0100

    random endpoint added

--- front-end/src/Container/RandomSelection/RandomSelection.tsx
+++ front-end/src/Container/RandomSelection/RandomSelection.tsx
@@ -1,9 +1,31 @@
+import MovieSimple from "../../Component/Movie/MovieSimple";
+import MovieType from "../../types/MovieType";
+import { useState, useEffect } from "react";
+
 const RandomSelection = () => {
-  return (
-    <div className="homePage">
-      <p>THIS WILL SHOW A RANDOM SELCTION OF MOVIES</p>
-    </div>
-  );
+  const [movie, setMovie] = useState<MovieType>();
+
+  const getRandomMovie = async (): Promise<void> => {
+    try {
+      let url = "http://localhost:8080/movie/random";
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("failed to fetch data");
+      }
+      const movieData = await response.json();
+      setMovie(movieData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("run this");
+    getRandomMovie();
+  }, []);
+
+  return <div className="randomMovie">{!movie ? <p> cannnot access movie database </p> : <MovieSimple movie={movie} />}</div>;
 };
 
 export default RandomSelection;
